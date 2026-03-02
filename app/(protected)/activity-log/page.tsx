b3d3641cf35163e9b05ca52db9bf84ae8e3bfaec
CxxJay <23-1-02121@vsu.edu.ph>
2026-03-02 20:53:27 +0800

    [feat] Create Activity Log page

---
+++ app/(protected)/activity-log/page.tsx
@@ -0,0 +1,10 @@
+export default function ActivityLogPage() {
+  return (
+    <div className="flex h-screen">
+      <main className="flex-1 p-6">
+        <h1 className="text-2xl font-bold mb-4">ActivityLog</h1>
+        {/* ActivityLog content goes here */}
+      </main>
+    </div>
+  );
+}
