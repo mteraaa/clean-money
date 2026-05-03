4b170dc9b33d238a4e9d670f995d60bf249b9d87
Mhac Alester Montera <mhacalester.montera@gmail.com>
2026-05-03 17:52:26 +0800

    [feat] Fetch fine_amount, total_students_with_fines, fineStudentsPaid

--- app/(protected)/(shared)/dashboard/types.ts
+++ app/(protected)/(shared)/dashboard/types.ts
@@ -2,6 +2,8 @@ export type Balance = {
   cash_on_bank: number;
   cash_on_hand: number;
   collectibles: number;
+  fine_amount: number;
+  total_students_with_fines: number;
   faculty_code: string | null;
   campus_code: string | null;
 };
